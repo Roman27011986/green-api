22af81d41b6a5038be9d8800fd2a0f155a1b7bc1
roman.nesterenko@siz.bz <didorsn@gmail.com>
2023-06-05 10:25:02 +0300

    hot fix

--- src/services/apiChat.js
+++ src/services/apiChat.js
@@ -3,24 +3,19 @@ axios.defaults.baseURL = "https://api.green-api.com/";
 
 export const sendMsg = async (crds, numb, text) => {
     const [id, token] = crds;
-    const trendMovies = await axios.post(`waInstance${id}/sendMessage/${token}`, {
+    await axios.post(`waInstance${id}/sendMessage/${token}`, {
         chatId: `${numb}@c.us`,
         message: text,
     });
-    console.log("sendMsg", trendMovies);
-    return trendMovies;
 };
 
 export const getMsg = async (crds) => {
     const [id, token] = crds;
-    const trendMovies = await axios.get(`waInstance${id}/receiveNotification/${token}`);
-
-    return trendMovies;
+    const response = await axios.get(`waInstance${id}/receiveNotification/${token}`);
+    return response;
 };
 
 export const delNot = async (crds, numb) => {
     const [id, token] = crds;
-    const trendMovies = await axios.delete(`waInstance${id}/deleteNotification/${token}/${numb}`);
-
-    return trendMovies;
+    await axios.delete(`waInstance${id}/deleteNotification/${token}/${numb}`);
 };
